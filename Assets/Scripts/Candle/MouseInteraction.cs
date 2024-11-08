d89de0ed69be43ccbdae95c325a1d67b2a84ca34
TheMakness <44522330+TheMakness@users.noreply.github.com>
2024-11-08 21:59:15 +0100

    Add mouse movement interaction

--- Assets/Scripts/Candle/MouseInteraction.cs
+++ Assets/Scripts/Candle/MouseInteraction.cs
@@ -1,19 +1,45 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MouseInteraction : MonoBehaviour
 {
-
-    
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    private MoveableObject selectedObject;
+    private Vector3 mousePosition;
+    private Vector3 mouseDirection;
+    private bool mouseLock;
         
-    }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        mousePosition = Input.mousePosition;
 
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                selectedObject = hit.transform.gameObject.GetComponent<MoveableObject>();
+                if (selectedObject != null)
+                    mouseLock = true;
+            }
+        }
+
+        if (!Input.GetMouseButton(0) && mouseLock)
+        {
+            mouseLock = false;
+            selectedObject = null;
+        }
+
+
+        if (mouseLock)
+        {
+            Vector3 screenPos = new Vector3(mousePosition.x, mousePosition.y, Camera.main.WorldToScreenPoint(selectedObject.transform.position).z);
+            Vector3 newLocation = Camera.main.ScreenToWorldPoint(screenPos);
+            selectedObject.transform.position = newLocation;
+        }
     }
+
+ 
+
 }
