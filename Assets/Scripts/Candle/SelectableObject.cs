df25c2d61927f9197f3d64e742cbf286e4188e5d
TheMakness <44522330+TheMakness@users.noreply.github.com>
2024-11-10 13:25:22 +0100

    Inverse rotation

--- Assets/Scripts/Candle/SelectableObject.cs
+++ Assets/Scripts/Candle/SelectableObject.cs
@@ -20,11 +20,11 @@ public class SelectableObject : MonoBehaviour
     {
         if (Vector3.Dot(transform.position, Vector3.up) >= 0)
         {
-            rotationAnchor.transform.Rotate(transform.up,Vector3.Dot(delta,Camera.main.transform.right),Space.World);
+            rotationAnchor.transform.Rotate(transform.up,-Vector3.Dot(delta,Camera.main.transform.right),Space.World);
         }
         else
         {
-            rotationAnchor.transform.Rotate(transform.up, -Vector3.Dot(delta, Camera.main.transform.right),Space.World);
+            rotationAnchor.transform.Rotate(transform.up, Vector3.Dot(delta, Camera.main.transform.right),Space.World);
         }
 
         rotationAnchor.transform.Rotate(Camera.main.transform.right,Vector3.Dot(delta, Camera.main.transform.up),Space.World);
